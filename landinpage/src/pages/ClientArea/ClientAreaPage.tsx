42e7968d53f2f960bf3bdbe67f6195b3b013f57d
Felipe <fcarmo072@gmail.com>
2026-03-14 00:09:09 -0300

    ajustando pagamento fallback da federacao2

--- landinpage/src/pages/ClientArea/ClientAreaPage.tsx
+++ landinpage/src/pages/ClientArea/ClientAreaPage.tsx
@@ -118,7 +118,15 @@ export function ClientAreaPage() {
                 paymentData = {
                     transaction_amount: 50,
                     payment_method_id: 'pix',
-                    payer: { email: user?.email }
+                    payer: { 
+                        email: user?.email,
+                        identification: {
+                            type: 'CPF',
+                            number: user?.cpf?.replace(/\D/g, '')
+                        },
+                        first_name: user?.name?.split(' ')[0] || 'Cliente',
+                        last_name: user?.name?.split(' ').slice(1).join(' ') || 'Evolução'
+                    }
                 };
             } else if (cardData) {
                 paymentData = cardData.formData;
